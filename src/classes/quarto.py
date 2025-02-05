9ea0008eb9d714ef71b57961c489f8541165365a
Alberto Rodrigues <allio.professional@gmail.com>
2025-02-04 23:01:54 -0300

    feat: adding numero quarto

--- src/classes/quarto.py
+++ src/classes/quarto.py
@@ -1,6 +1,7 @@
 class Quarto:
-    def __init__(self, nome, valor_dia, ocupacao_total, hospedes):
+    def __init__(self, nome, numero, valor_dia, ocupacao_total, hospedes):
         self.nome = nome
+        self.numero = numero
         self.valor_dia = valor_dia
         self.ocupacao_total = ocupacao_total
         self.hospedes = hospedes
