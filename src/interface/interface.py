e03418ad462a98648948295e3a59def0559e716f
Alberto Rodrigues <allio.professional@gmail.com>
2025-02-07 11:43:35 -0300

    feat: estruturando pasta de interface

---
+++ src/interface/interface.py
@@ -0,0 +1,13 @@
+# mostra usuários alocados em quartos do hotel
+def mostrar_dashboard_checkin():
+    return
+
+# mostra usuários que não estão mais no hotel
+def mostrar_dashboard_checkout():
+    return
+
+def mostrar_dashboard_agendamentos():
+    return
+
+def menu():
+    return
